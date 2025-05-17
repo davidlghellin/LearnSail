7acc1271b572c9688bfc88ab27f5a01401da69f2
David López <davidlghellin@gmail.com>
2025-05-17 12:35:42 +0200

    Primer ejemplo y configuración estructura

---
+++ client/client.py
@@ -0,0 +1,12 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, expr, lit
+import random
+
+spark = SparkSession.builder.remote("sc://sail-spark-server.sail.svc.cluster.local:50051").getOrCreate()
+
+
+print("✅ Conectado a Spark:", spark.version)
+
+df = spark.range(2)
+df.show()
+spark.stop()
